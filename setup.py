a7332106fc71fb2ad31ee6411593de9eff8273aa
Bruno Daniel <30147951+bdanielby@users.noreply.github.com>
2023-04-21 16:59:04 +0200

    Make the project pypi-installable.

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+github_url = 'https://github.com/bdanielby/mlpj'
 
 setup(
     name='mlpj',
-    version='0.1.0',    
+    version='0.1.1',
     description='Tools for machine learning projects',
-    url='https://github.com/bdanielby/mlpj',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url=github_url,
     author='Bruno Daniel',
     license='MIT',
     packages=['mlpj'],
     install_requires=[
         'numpy', 'pandas', 'matplotlib'
     ],
-
+    project_urls={
+        'Homepage': github_url,
+        'Source': github_url,
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
