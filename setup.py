2d934f770cfc18a0a63a4fc65a8dda656b0aa6a1
Bruno Daniel <30147951+bdanielby@users.noreply.github.com>
2023-07-24 23:11:58 +0200

    version number 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ github_url = 'https://github.com/bdanielby/mlpj'
 
 setup(
     name='mlpj',
-    version='0.2.4',
+    version='0.3.0',
     description='Tools for machine learning projects',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
